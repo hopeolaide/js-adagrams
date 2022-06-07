7071b05df2e176fe92b00291c332a94b225c4cf2
Hope Olaide <hopeolaide@gmail.com>
2022-06-07 07:07:34 -0400

    Wave 01 complete.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,24 @@
+import {letterPool, scoreChart} from './letters'
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let letterOptions = []; 
+  
+  for (const [letter, frequency] of Object.entries(letterPool)) {
+    for (let i = 0; i < frequency; i++) {
+      letterOptions.push(letter);
+    }
+  }
+  
+  let userLetters = [];  //This was origianlly done with a deepcopy method in python 
+  while (userLetters.length < 10) {
+    const letter_index = Math.floor(Math.random() * letterOptions.length);
+    const letter_draw = letterOptions[letter_index];
+    letterOptions.splice(letter_index, 1);
+    userLetters.push(letter_draw);
+  }
+              
+  return userLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
